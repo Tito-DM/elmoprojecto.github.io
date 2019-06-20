93fbaa1694e1c3aff3ba1897e1a723a58fa5881e
tito Domingos <muandatito@gmail.com>
2019-06-20 20:52:46 +0200

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,11 @@
+//zoom img
+
+$(document).ready(function () {
+  $('.materialboxed').materialbox();
+  $(".button-collapse").sideNav();
+  $('.modal').modal();
+});
+
+$(document).ready(function () {
+  $('select').material_select();
+});
